93682b8a8480c12e6f7af75af1ac472e85f52187
Alexei Schwab <alexeischwab@gmail.com>
2024-07-02 15:35:15 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# event venue catchment area
- Generate catchment areas based on travel time from a UK postcode
+# Event venue catchment area
+Generate catchment areas based on travel time from a UK postcode
+
+This Streamlit app lets you generate a list of postcodes that are reachable from a starting point. 
+Reachable areas can be exported as a list of postcodes, to match against supporter records
+for more accurate comms targetting. 
+
+## Usage 
+1. Enter the event postcode. 
+   _The postcode must be a valid and correctly formatted UK postcode_
+2. Enter the travel time in minutes (default is 60)
+   _The app will search for postcodes reachable by driving for this amount of time
+   under normal driving conditions_
+3. Enter a Mapbox token
+   _The app uses the Mapbox API to generate the search results. If you don't already
+   have a token, create a Mapbox account and follow [these instructions](https://docs.mapbox.com/help/getting-started/access-tokens/) 
+   to get a token._
+4. Click the Search button to get the results 
+   _The Search button will be inactive until all parameters are provided_
+5. Results will be displayed on a map. To export the results as a CSV, click 
+   the button below the map.
+
+
+## Contribute
+If you notice any bugs or have suggestions for improvements, please open an issue. 
+
+## Credits
+This application uses the [ONS Postcode directory](https://geoportal.statistics.gov.uk/datasets/a8a2d8d31db84ceea45b261bb7756771/about) which contains data from Royal Mail, Gridlink, LPS (Northern Ireland), Ordnance Survey and ONS Intellectual Property Rights.
+* Contains OS data © Crown copyright and database right 2024
+* Contains Royal Mail data © Royal Mail copyright and database right 2024
+* Source: Office for National Statistics licensed under the Open Government Licence v.3.0
---
+++ st_event_travel.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import requests
+
+st.title('Event travel time coverage')
+
+@st.cache_data
+def load_postcodes(file): 
+	pcds_df = pd.read_parquet(file)
+	return pcds_df
+
+if 'disabled' not in st.session_state: 
+	st.session_state.disabled = True
+
+with st.spinner("Loading postcode data"): 
+	postcodes = load_postcodes('./onspd_may24.parquet')
+	postcodes_gdf = gpd.GeoDataFrame(postcodes, geometry= gpd.points_from_xy(postcodes.long, postcodes.lat))
+
+if postcodes is not None: 
+	# Get venue postcode input
+	venue_postcode = st.text_input(label="Venue postcode (required)", value= None)
+
+	# Get travel time input
+	travel_time = st.number_input(label="Travel time (mins)", value=60, step=10)
+
+	# Get Mapbox API key input
+	token = st.text_input(label="Mapbox token (required)", type="password")
+
+	# help info - where to get a token 
+	st.info("How to get a Mapbox token: https://docs.mapbox.com/help/getting-started/access-tokens/")
+
+	valid_postcode = False 
+
+	# validate postcode 
+	if type(venue_postcode) == None or venue_postcode == "": 
+		valid_postcode = False
+	elif type(venue_postcode) != None and ~postcodes.pcds.isin([venue_postcode]).any():	
+		valid_postcode = False
+	elif postcodes.pcds.isin([venue_postcode]).any(): 
+		valid_postcode=True
+
+
+	# Lookup venue lat/long 
+	pcds = postcodes.set_index('pcds')
+
+if valid_postcode == True:
+	longitude, latitude = pcds.at[venue_postcode, 'long'], pcds.at[venue_postcode, 'lat']
+	# Set API request parameters
+	url = f'https://api.mapbox.com/isochrone/v1/mapbox/driving/{longitude}%2C{latitude}?contours_minutes={travel_time}&polygons=true&access_token={token}'
+
+if token and venue_postcode: 
+	st.session_state.disabled = False
+
+if st.button(label="Search", type="primary", disabled=st.session_state.disabled):
+		# Send API request
+		r = requests.get(url)
+		# Create GeoDataFrame from API response 
+		gdf = gpd.GeoDataFrame.from_features(r.json()['features'])
+		# Spatial join the reachable area to the postcode point locations
+		reachable_postcodes = postcodes_gdf.sjoin(gdf)
+		# Display map of results
+		st.map(reachable_postcodes, latitude='lat', longitude='long')
+		# Button to generate CSV of reachable postcodes
+		st.download_button(label="Download postcodes as CSV", data=reachable_postcodes.to_csv(columns=["pcds"], index=False), 
+			file_name=venue_postcode+"_reachable_postcodes.csv", 
+			mime="text/csv", type="secondary")
+
+		
